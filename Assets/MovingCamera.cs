367324e01e7b05140e87cba396019a743f9dfa22
shshwdr <cs.vivian04@gmail.com>
2019-01-28 01:53:13 -0800

    add seed and branch

---
+++ Assets/MovingCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{
+    public float movingSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y- movingSpeed * Time.deltaTime,transform.position.z);
+    }
+}
---
+++ Assets/Seed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float force = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 touchPosition;
+        if (Input.GetMouseButton(0))
+        {
+            touchPosition = Input.mousePosition;
+        } else if (Input.touchCount == 1)
+        {
+            touchPosition = Input.GetTouch(0).position;
+        }
+        else
+        {
+            return;
+        }
+        touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+        Vector2 forceVector = (transform.position - touchPosition).normalized * force;
+        rb.AddForce(forceVector);
+    }
+}
