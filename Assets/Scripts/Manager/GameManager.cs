7e632dd7725ea0f32a3c436c88e497ba8c830cc4
shshwdr <cs.vivian04@gmail.com>
2019-02-05 02:09:50 -0800

    add collect star logic

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -7,17 +7,24 @@ public class GameManager : Singleton<GameManager>
 {
     public bool IsGameOver { get; private set; }
     public bool IsWin { get; private set; }
+    int starNum;
     // Start is called before the first frame update
     void Start()
     {
         IsGameOver = false;
         IsWin = false;
+        starNum = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GetStar()
+    {
+        starNum++;
     }
 
     public void Win()
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    Transform starCollectTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starCollectTarget = GameObject.Find("starCollectTarget").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "seed")
+        {
+            GameManager.Instance.GetStar();
+            GetComponent<PolygonCollider2D>().enabled = false;
+            Canvas c;
+            Vector3 starMoveTarget = Camera.main.ScreenToWorldPoint(starCollectTarget.position);
+            GetComponent<Rigidbody2D>().MovePosition(starMoveTarget);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
