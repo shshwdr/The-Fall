07a7023127441888b4101433fc1e09653112b08e
shshwdr <cs.vivian04@gmail.com>
2019-02-06 02:21:45 -0800

    star move to the top

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DigitalRuby.Tween;
 
 public class Star : MonoBehaviour
 {
@@ -11,15 +12,37 @@ public class Star : MonoBehaviour
         starCollectTarget = GameObject.Find("starCollectTarget").transform;
     }
 
+    private void TweenMove()
+    {
+        System.Action<ITween<Vector3>> updateCirclePos = (t) =>
+        {
+            GetComponent<Rigidbody2D>().MovePosition(t.CurrentValue);
+        };
+
+        System.Action<ITween<Vector3>> circleMoveCompleted = (t) =>
+        {
+            Debug.Log("star move completed");
+            Destroy(gameObject);
+        };
+
+        Vector3 currentPos = transform.position;
+        Vector3 starMoveTarget = Camera.main.ScreenToWorldPoint(starCollectTarget.position);
+        //currentPos.z = startPos.z = midPos.z = endPos.z = 0.0f;
+
+        // completion defaults to null if not passed in
+        gameObject.Tween("MoveCircle", currentPos, starMoveTarget, 1f, TweenScaleFunctions.CubicEaseIn, updateCirclePos, circleMoveCompleted);
+            
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "seed")
         {
             GameManager.Instance.GetStar();
             GetComponent<PolygonCollider2D>().enabled = false;
-            Canvas c;
-            Vector3 starMoveTarget = Camera.main.ScreenToWorldPoint(starCollectTarget.position);
-            GetComponent<Rigidbody2D>().MovePosition(starMoveTarget);
+            TweenMove();
+            //Vector3 starMoveTarget = Camera.main.ScreenToWorldPoint(starCollectTarget.position);
+            //GetComponent<Rigidbody2D>().MovePosition(starMoveTarget);
         }
     }
 
