32f4e36df72be4c9caf2d1d3dda4af0dd1c8bed4
shshwdr <cs.vivian04@gmail.com>
2019-02-23 23:59:29 -0800

    keep working on lightning

--- Assets/Lightning.cs
+++ Assets/Lightning.cs
@@ -11,23 +11,42 @@ public class Lightning : MonoBehaviour
     public LightningBoltScript[] lightningBolt;
     public GameObject screenLight;
 
+    Color lightFadeoutColor;
+    Color lightFullColor;
+
+    float countTime;
+
+    float lightFadeInTime = 0.5f;
+    float lightStayTime = 1f;
+    float lightFadeOutTime = 1f;
+
     enum LightningState { inactive,showLight,fadeoutLight,showLightning,screenLight,end};
     LightningState state;
     // Start is called before the first frame update
     void Start()
     {
         
-
-
-        state = LightningState.showLight;
+        state = LightningState.inactive;
         light.SetPosition(0, startPoint.position);
         light.SetPosition(1, endPoint.position);
-        foreach(LightningBoltScript bolt in lightningBolt)
+        lightFullColor = light.startColor;
+        lightFadeoutColor  = new Color(light.startColor.r, light.startColor.g, light.startColor.b, 0);
+
+        foreach (LightningBoltScript bolt in lightningBolt)
         {
 
             bolt.StartPosition = startPoint.position;
             bolt.EndPosition = endPoint.position;
         }
+
+        StartCoroutine(StartActivity());
+    }
+
+    IEnumerator StartActivity()
+    {
+        yield return new WaitForSeconds(1);
+        state = LightningState.showLight;
+        countTime = 0;
     }
 
 
@@ -35,11 +54,25 @@ public class Lightning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(state);
         switch (state)
         {
             case LightningState.inactive:
+                light.startColor = lightFadeoutColor;
+                light.endColor = lightFadeoutColor;
+                //lightningBolt
                 break;
             case LightningState.showLight:
+                countTime += Time.deltaTime;
+                if (countTime < lightFadeInTime)
+                {
+                    light.startColor = Color.Lerp(lightFadeoutColor, lightFullColor,countTime/lightFadeInTime);
+                    light.endColor = light.startColor;
+                }
+                if (countTime > lightFadeInTime + lightStayTime)
+                {
+                    state = LightningState.fadeoutLight;
+                }
                 break;
             case LightningState.fadeoutLight:
                 break;
