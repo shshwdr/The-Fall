361f8f933b7e47a53d572ac7f51001ff09cf907a
shshwdr <cs.vivian04@gmail.com>
2019-03-19 02:19:35 -0700

    add webline on spider

---
+++ Assets/LineRenderConnection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRenderConnection : MonoBehaviour
+{
+    public Transform p1;
+    public Transform p2;
+    LineRenderer line;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        line.SetPositions(new Vector3[] { p1.position, p2.position });
+    }
+}
