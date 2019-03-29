315c125f293636dee7a860c7bf10bd9698f4f962
shshwdr <cs.vivian04@gmail.com>
2019-03-29 02:06:02 -0700

    add dropdown box for gameplay

---
+++ Assets/GamePlayViewController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayViewController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SelectDropdown(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                Loginin();
+                break;
+            case 1:
+                Leaderboard();
+                break;
+            case 2:
+                Achievement();
+                break;
+            case 3:
+                Logout();
+                break;
+            default:
+                Debug.LogError("index " + index + " does not support in dropdown box");
+                break;
+        }
+    }
+    public void Loginin()
+    {
+        Debug.Log("Loginin");
+
+    }
+    public void Logout()
+    {
+        Debug.Log("Logout");
+
+    }
+    public void Leaderboard()
+    {
+        Debug.Log("Leaderboard");
+    }
+    public void Achievement()
+    {
+        Debug.Log("Achievement");
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
